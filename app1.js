8ca69b11bf6c1a7583c629a88312f9fe73c0ca65
unknown <golovanov911@gmail.com>
2022-01-16 15:50:48 +0300

    v2

---
+++ app1.js
@@ -0,0 +1,8 @@
+const da = 'да';
+const vopros = prompt('Вы хотите принять меня на работу? (да/нет)');
+
+if(vopros === 'да'){
+   console.log(alert('Напишите мне в телеграмм @sdpctr'))}
+else{ vopros === 'нет'
+   console.log(alert('Хорошего вам дня!'))
+};
