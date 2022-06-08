3b65766c9a645cd2ec895915e85e1f4739faf8f7
unknown <golovanov911@gmail.com>
2022-06-08 20:34:52 +0300

    Q

--- app1.js
+++ app1.js
@@ -1,16 +1,18 @@
 const da = 'да';
-const vopros = prompt('Вы хотите принять меня на работу? (да/нет)');
+const vopros = prompt('Вы хотите принять меня на работу?');
 
-if(vopros === 'да'){
-   console.log(alert('Напишите мне в телеграмм @sdpctr'))}
-else{ vopros === 'нет'
-   console.log(alert('Хорошего вам дня!'))
-};
+if (vopros == 'да') {
+  console.log(alert('Напишите мне в телеграмм @сdpctr'));
+} else if (vopros == 'нет') {
+  console.log(alert('В любом случае был рад встретить вас здесь!'));
+} else {
+  ('Я не очень понял ваш ответ, но если у вас остались ко мне вопросы свяжитесь со мной в телеграмм @cdpctr');
+}
 
 let x;
 
-x=`
+x = `
  <h1>Hello world!</h1>
 
-`
-document.body.innerHTML=x;
+`;
+document.body.innerHTML = x;
